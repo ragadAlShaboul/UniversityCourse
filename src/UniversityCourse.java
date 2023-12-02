c0b4cd4bc6e610b6e812d3dac616c5dd64870d41
RagadAlShaboul <ragad.alshaboul@gmail.com>
2023-12-02 12:07:16 +0300

    UniversityCourse uploaded by Sr.Hamza

---
+++ src/UniversityCourse.java
@@ -0,0 +1,52 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class UniversityCourse {
+    private  String courseName;
+    private String instructorName;
+    private ArrayList<String> studentNames = new ArrayList<String>();
+    public UniversityCourse(String courseName,String instructorName){
+        this.courseName = courseName;
+        this.instructorName = instructorName;
+    }
+    public void register(String studentName){
+        if(studentNames == null){
+            throw new IllegalArgumentException();
+        }
+        this.studentNames.add(studentName);
+    }
+    public void register(String[] studentNames){
+        if(studentNames == null){
+            throw new IllegalArgumentException();
+        }
+        for(int i=0;i<studentNames.length;i++){
+            this.studentNames.add(studentNames[i]);
+        }
+    }
+    //    public void drop(String studentName){
+//        if(studentName == null || !isRegistered(studentName)){
+//            throw new IllegalArgumentException();
+//        }
+//
+//        this.studentNames.remove(studentName);
+//
+//    }
+//    public void drop(String[] studentNames){
+//        if(studentNames == null){
+//            throw new IllegalArgumentException();
+//        }
+//        for(int i=0;i<studentNames.length;i++){
+//            if(isRegistered(studentNames[i])) {
+//                this.studentNames.remove(studentNames[i]);
+//            }
+//        }
+//
+//    }
+    public boolean isRegistered(String studentName){
+        return this.studentNames.contains(studentName);
+    }
+    public int getNumberOfRegisteredStudents(){
+        return this.studentNames.size();
+    }
+
+}
